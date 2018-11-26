c77409dda99ffd2dc8c85c065eafbd6e463a01a3
Patrik Votocek <patrik@votocek.cz>
2018-11-27 00:00:37 +0100

    init typescript

---
+++ typescript.js
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: [
+        './src/index.js',
+        './src/typescript.js',
+    ],
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+        'import/parsers': {
+            'typescript-eslint-parser': [
+                '.ts',
+                '.tsx',
+            ],
+        },
+    },
+};
