d42ff7e67b46d2ebe7c7053465ccb27ddaa1c536
Martin Štekl <martin.stekl@gmail.com>
2019-05-24 10:08:42 +0200

    fix native tab indent

--- src/filenames.js
+++ src/filenames.js
@@ -1,8 +1,8 @@
 module.exports = {
-    plugins: [
-        'filenames',
-    ],
-    rules: {
-        'filenames/match-regex': ['error', '^[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)*$'],
-    },
+	plugins: [
+		'filenames',
+	],
+	rules: {
+		'filenames/match-regex': ['error', '^[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)*$'],
+	},
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true,
-        jasmine: true,
-        jest: true,
-    },
-    extends: [
-        './native.js',
-        './filenames.js',
-        './import.js',
-    ],
+	env: {
+		es6: true,
+		node: true,
+		jasmine: true,
+		jest: true,
+	},
+	extends: [
+		'./native.js',
+		'./filenames.js',
+		'./import.js',
+	],
 };
