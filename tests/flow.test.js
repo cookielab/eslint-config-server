0f844839a4581e145c50799c7ee610b02d9cb31d
Martin Štekl <martin.stekl@gmail.com>
2019-10-10 16:41:29 +0200

    add tests of configuration against rule schema

--- tests/flow.test.js
+++ tests/flow.test.js
@@ -1,5 +1,11 @@
+const Ajv = require('ajv');
 const localConfig = require('../src/flow');
 const plugin = require('eslint-plugin-flowtype');
+
+const ajv = new Ajv({
+	allErrors: true,
+	useDefaults: true,
+});
 
 describe('Rules Configurations', () => {
 	describe('flow', () => {
@@ -17,5 +23,24 @@ describe('Rules Configurations', () => {
 
 			expect(extraLocalRules).toEqual([]);
 		});
+
+		const resolvedRules = plugin.rules;
+		for (const ruleName of Object.keys(resolvedRules)) {
+			const rule = resolvedRules[ruleName];
+			if (rule.meta != null && rule.meta.schema != null && Array.isArray(rule.meta.schema) && rule.meta.schema.length > 0) {
+				it(`rule "${ruleName}" has valid configuration`, async () => {
+					const schemas = rule.meta.schema;
+					const localRule = localConfig.rules[ruleName];
+					if (Array.isArray(localRule) && localRule.length > 1) {
+						const configurations = localRule.slice(1);
+						for (let index = 0; index < configurations.length; index++) {
+							const schema = schemas[index];
+							const configuration = configurations[index];
+							await ajv.validate(schema, configuration);
+						}
+					}
+				});
+			}
+		}
 	});
 });
--- tests/native.test.js
+++ tests/native.test.js
@@ -1,10 +1,17 @@
+const Ajv = require('ajv');
 const localConfig = require('../src/native');
-const plugin = require('eslint/conf/eslint-all');
+const eslintAll = require('eslint/conf/eslint-all');
+const rules = require('eslint/lib/rules');
+
+const ajv = new Ajv({
+	allErrors: true,
+	useDefaults: true,
+});
 
 describe('Rules Configurations', () => {
 	describe('native', () => {
 		const localRules = Object.keys(localConfig.rules);
-		const pluginRules = Object.keys(plugin.rules);
+		const pluginRules = Object.keys(eslintAll.rules);
 
 		it('has configuration for all supported rules', () => {
 			const notConfiguredPluginRules = pluginRules.filter((ruleName) => !localRules.includes(ruleName));
@@ -17,5 +24,23 @@ describe('Rules Configurations', () => {
 
 			expect(extraLocalRules).toEqual([]);
 		});
+
+		const resolvedRules = rules.entries();
+		for (const [ruleName, rule] of resolvedRules) {
+			if (rule.meta != null && rule.meta.schema != null && Array.isArray(rule.meta.schema) && rule.meta.schema.length > 0) {
+				it(`rule "${ruleName}" has valid configuration`, async () => {
+					const schemas = rule.meta.schema;
+					const localRule = localConfig.rules[ruleName];
+					if (Array.isArray(localRule) && localRule.length > 1) {
+						const configurations = localRule.slice(1);
+						for (let index = 0; index < configurations.length; index++) {
+							const schema = schemas[index];
+							const configuration = configurations[index];
+							await ajv.validate(schema, configuration);
+						}
+					}
+				});
+			}
+		}
 	});
 });
